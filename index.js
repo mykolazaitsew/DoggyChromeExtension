be19b1c11d833f4b1e56861e0b32ef7f49a12bb4
Kolya Zaitsev <zaitsev.nikolay11@gmail.com>
2025-02-23 18:14:39 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+let count = 0;
+
 document.getElementById('doggo').addEventListener('click', function () {
-    document.getElementById('bark_sound').play();
+    let doggo = document.getElementById('doggo');
+    let sounds = [document.getElementById('bark1'), document.getElementById('bark2')];
+    let clickCounter = document.getElementById('clickCount');
+
+    doggo.src = "shibwow.png";
+
+    let randomSound = sounds[Math.floor(Math.random() * sounds.length)];
+    randomSound.play();
+
+    doggo.classList.add('shake');
+
+    count++;
+    clickCounter.textContent = "Woofs: " + count;
+
+
+    let soundDuration = randomSound.duration * 1000 || 2000;
+
+    setTimeout(() => {
+        doggo.classList.remove('shake');
+        doggo.src = "shiba.png";
+    }, soundDuration);
 });
